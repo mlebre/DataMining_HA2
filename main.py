bf7963dbec52a63454955a56a591998071cee665
Marianne Lebre <marianne.lebre@insa-lyon.fr>
2016-11-17 19:37:39 +0100

    Many changes: set is now a list, process recognizes pairs/more of items

--- main.py
+++ main.py
@@ -23,14 +23,14 @@ class APriori:
 		''' Counting of self.processing-shingle thanks to the subset of size k-1
 		'''
 		fi=open(self.name, 'r')
-		self.set=dict() # deletion of the previous set
+		self.set=list() # deletion of the previous set
 		for line in fi:
 			line=line.split(' ')
 			line.remove('\n')
 			if self.processing==0:
 				self.firstProcess(line)
 			else:
-				self.process()
+				self.process(line)
 		self.processing+=1
 		fi.close()
 		
@@ -40,31 +40,35 @@ class APriori:
 		'''
 		i=0
 		while i+self.processing < len(line):
-			if line[i] not in self.set.keys():
+			if line[i] not in self.set:
 				# item not yet appear
-				self.set[line[i]]=1
+				self.set.append(line[i])
+				self.set.append(1)
 			else:
-				self.set[line[i]]=self.set[line[i]]+1
+				self.set[self.set.index(line[i])+1]=self.set[self.set.index(line[i])+1]+1
 			i+=1
 
 	def process(self,line):
-		i=0
-		while i+self.processing<len(line):
-			j=i+1
-			while j+self.processing<len(line):
-				if self.line[i:i+self.processing] and self.line[j:j+self.processing] in self.freq_subset:
-					self.set[line] 
-				j+=1
-			i+=1
+		print '\nPROCESS'
+		for i in xrange(len(line)-self.processing):
+			item1=line[i:i+self.processing]
+			if str(item1).strip('[]\'') in self.freq_subset:				
+				for j in xrange(i+1,len(line)-self.processing):
+					item2=line[j:j+self.processing]
+					if str(item2).strip('[]\'') in self.freq_subset:
+						print item1, item2
+
+				
 
 
 	def pass2(self):
 		self.freq_subset=list()
-		for i,e in enumerate(self.set.keys()):
-			if float(self.set[e])/len(self.set)>=self.s:
-				self.freq_subset.append(self.set.keys()[i])
-				self.freq_subset.append(self.set[e])
-		print self.freq_subset
+		i=0
+		while i <len(self.set):
+			if float(self.set[i+1])/(len(self.set)/(self.processing+1))>=self.s: 
+			# Dividing with self.process+1 because set contains the count of each item
+				self.freq_subset.append(self.set[i])
+			i+=2
 
 
 
@@ -87,7 +91,9 @@ T=0.05
 AP=APriori(file_name,2,T)
 AP.pass1()
 print 'Singleton search'
-print AP.set
+#print AP.set
 print 'Frequent item set'
 print 'threshold:', AP.s
 AP.pass2()
+print AP.freq_subset
+AP.pass1()
